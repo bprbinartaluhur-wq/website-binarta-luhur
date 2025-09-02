511071ddd3f349b05fa1c728367081dcd18af5bc
Binarta Luhur <bprbinartaluhur@gmail.com>
2025-09-02 02:11:28 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,12 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "firebasestorage.googleapis.com",
       },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
