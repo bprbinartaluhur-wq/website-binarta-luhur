5334853a99d7752151b314af51a5ad7360c20d40
Binarta Luhur <bprbinartaluhur@gmail.com>
2026-01-03 00:53:24 +0000

    buat jarak sedikit di antara kartu tersebut

--- src/components/sections/products.tsx
+++ src/components/sections/products.tsx
@@ -29,7 +29,7 @@ const productItems = [
 export default function Products() {
   return (
     <section id="produk" className="w-full">
-      <div className="grid grid-cols-1 md:grid-cols-3">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-1">
         {productItems.map((item) => (
           <Link href={item.link} key={item.title} className="group relative block overflow-hidden h-96 md:h-auto md:aspect-[3/4]">
             <Image
